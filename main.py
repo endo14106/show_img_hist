5fb613bcfcc9f11f40a8d7d0f0e455385e1747df
Fumiya Endo <f.endo.849@stn.nitech.ac.jp>
2022-07-21 14:18:09 +0900

    modify code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from skimage.io import imread
 # download the image
 img_url = 'https://www.soccer-king.jp/wp-content/uploads/2019/11/9.jpg'
 
-im = imread(img_url)
+im = imread(img_url))
 
 st.image(im, caption='image from wikimedia commons',
          use_column_width=True)
